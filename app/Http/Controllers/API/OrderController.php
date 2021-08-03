0bf17cb2216bf5a57ea1b63e6e3ba8393f353bd6
omarhabeh <l6h2010@hotmail.com>
2021-08-03 10:15:22 +0300

    testing - 2

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -237,7 +237,7 @@ class OrderController extends Controller
             // "&billing.city=$city" .
             // "&billing.postcode=$postcode" .
             // "&billing.country=$countryCode" .
-            // "&currency=SAR" .
+            "&currency=SAR" .
             "&paymentType=DB";
             // ."&testMode=EXTERNAL";
 
