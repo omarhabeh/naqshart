afa689b59ef04e3d9f43b158365e947574e408ff
Phares <35468773+PharesAbanmy@users.noreply.github.com>
2022-01-10 16:33:17 +0300

    Update order email

--- resources/views/emails/order.blade.php
+++ resources/views/emails/order.blade.php
@@ -90,9 +90,8 @@ margin-top: -60px;">Order #{{$order->id}}</small>
 
 <h1 style="color: black"> Thank you for your order! </h1>
 <p>
-    Hi  {{$order->fname}},
-We hope you enjoyed the digital gallery; We will send you tracking information when
-the order ships, you can find your order information bellow
+    Hi  {{$order->fname}},<br>
+We hope you enjoyed our digital gallery. The tracking information will be shared once the order ships, you can find your order details below:
 </p>
     <hr>
     <h2 style="color: black">Order Summary</h2>
